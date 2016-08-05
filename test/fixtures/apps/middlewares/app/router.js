8ea5700d19cea45d896f6b59ccf7c73c8d14b203
fengmk2 <m@fengmk2.com>
2016-08-05 23:56:15 +0800

    fix: should set X-Powered-By and X-Server-Id when controller error

--- test/fixtures/apps/middlewares/app/router.js
+++ test/fixtures/apps/middlewares/app/router.js
@@ -1,8 +1,9 @@
 'use strict';
 
-module.exports = function(app) {
+module.exports = app => {
   app.get('/', app.controller.home);
   app.get('/traceClient', app.controller.trace.traceClient);
 
   app.get('/traceServer', app.controller.trace.traceServer);
+  app.get('/error', app.controller.error);
 };
