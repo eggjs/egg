9fa1c60a5f4a17e4b1dd6f1d3e77b72fdae3f6b2
dead-horse <dead_horse@qq.com>
2016-08-27 00:21:21 +0800

    fix(meta): remove server-id

--- test/lib/core/app/middleware/meta.test.js
+++ test/lib/core/app/middleware/meta.test.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const should = require('should');
 const mm = require('egg-mock');
 const request = require('supertest');
 const utils = require('../../../../utils');
@@ -8,7 +7,6 @@ const utils = require('../../../../utils');
 describe('test/lib/core/app/middleware/meta.test.js', () => {
   let app;
   before(() => {
-    mm(process.env, 'HOSTNAME', 'appname-1-1');
     app = utils.app('apps/middlewares');
     return app.ready();
   });
@@ -29,38 +27,6 @@ describe('test/lib/core/app/middleware/meta.test.js', () => {
       .get('/error')
       .expect('X-Powered-By', 'Egg')
       .expect(500);
-  });
-
-  it('should hide X-Powered-By header', () => {
-    mm(app, 'poweredBy', false);
-    return request(app.callback())
-      .get('/')
-      .expect(res => {
-        should.not.exist(res.headers['X-Powered-By']);
-      })
-      .expect(200);
-  });
-
-  it('should get X-Server-Id header', () => {
-    return request(app.callback())
-      .get('/')
-      .expect('X-Server-Id', '1-1')
-      .expect(200);
-  });
-
-  it('should hide X-Server-Id header', done => {
-    mm(process.env, 'HOSTNAME', '');
-    const app = utils.app('apps/middlewares');
-    request(app.callback())
-    .get('/')
-    .expect(res => {
-      should.not.exist(res.headers['X-Server-Id']);
-    })
-    .expect(200, err => {
-      app.close();
-      should.not.exist(err);
-      done();
-    });
   });
 
   it('should get X-Readtime header', () => {
