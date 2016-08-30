263be3763510ca95cad1c3a547637e250333d2b1
fengmk2 <m@fengmk2.com>
2016-08-31 01:56:38 +0800

    test: add benchmark results

--- benchmarks/simple_view/config/config.prod.js
+++ benchmarks/simple_view/config/config.prod.js
@@ -7,3 +7,10 @@ exports.keys = 'foo';
 exports.logger = {
   dir: path.join(__dirname, '../logs'),
 };
+
+exports.alinode = {
+  enable: !!process.env.ALINODE_ENABLE,
+  appid: process.env.ALINODE_APPID,
+  server: process.env.ALINODE_SERVER,
+  secret: process.env.ALINODE_SECRET,
+};
