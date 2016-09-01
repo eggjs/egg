01922505fd0d74eb8ec439303a832647316d58c7
dead-horse <dead_horse@qq.com>
2016-09-01 21:17:53 +0800

    test: update benchmark scripts

--- benchmarks/simple_view/run.sh
+++ benchmarks/simple_view/run.sh
@@ -5,22 +5,29 @@ EGG_SERVER_ENV=prod node `dirname $0`/dispatch.js $1 &
 pid=$!
 
 sleep 3
-
-curl 'http://127.0.0.1:7001/' | grep 'title'
-echo ""
-curl 'http://127.0.0.1:7002/' | grep 'title'
-echo ""
-
 echo "------- egg view -------"
+curl 'http://127.0.0.1:7001/' -s | grep 'title'
+echo ""
 wrk 'http://127.0.0.1:7001/' \
   -d 10 \
   -c 50 \
   -t 8
 
+sleep 3
 echo "------- koa view -------"
+curl 'http://127.0.0.1:7002/' -s | grep 'title'
+echo ""
 wrk 'http://127.0.0.1:7002/' \
   -d 10 \
   -c 50 \
   -t 8
 
+sleep 3
+echo "------- toa view -------"
+curl 'http://127.0.0.1:7003/' -s | grep 'title'
+echo ""
+wrk 'http://127.0.0.1:7003/' \
+  -d 10 \
+  -c 50 \
+  -t 8
 kill $pid
