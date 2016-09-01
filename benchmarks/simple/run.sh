b216245cde074c4749769c4cf3a708a6459c0269
Yiyu He <dead-horse@users.noreply.github.com>
2016-09-01 22:44:08 +0800

    test: update benchmark scripts (#79)

--- benchmarks/simple/run.sh
+++ benchmarks/simple/run.sh
@@ -5,20 +5,28 @@ EGG_SERVER_ENV=prod node `dirname $0`/dispatch.js $1 &
 pid=$!
 
 sleep 3
-
+echo "------- egg hello -------"
 curl 'http://127.0.0.1:7001/'
 echo ""
-curl 'http://127.0.0.1:7002/'
-echo ""
-
-echo "------- egg hello -------"
 wrk 'http://127.0.0.1:7001/' \
   -d 10 \
   -c 50 \
   -t 8
 
+sleep3
 echo "------- koa hello -------"
+curl 'http://127.0.0.1:7002/'
+echo ""
 wrk 'http://127.0.0.1:7002/' \
+  -d 10 \
+  -c 50 \
+  -t 8
+
+sleep3
+echo "------- toa hello -------"
+curl 'http://127.0.0.1:7003/'
+echo ""
+wrk 'http://127.0.0.1:7003/' \
   -d 10 \
   -c 50 \
   -t 8
