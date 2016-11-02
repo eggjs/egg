0763dd14de009498571d62ab3f7b6121893f49bf
popomore <sakura9515@gmail.com>
2016-11-02 15:37:53 +0800

    feat: print error to console on unittest env
    consoleLevel on unittest is WARN, so it should be print,
    but egg disable console after app ready.
    
    so enable console on unittest env
    
    Closes eggjs/egg#127

--- config/config.local.js
+++ config/config.local.js
@@ -2,13 +2,11 @@
 
 module.exports = {
   logger: {
-    // 开发环境，将 INFO 以上级别的应用日志和 WARN 以上级别的系统日志输出到 stdout
     level: 'DEBUG',
     consoleLevel: 'INFO',
     coreLogger: {
       consoleLevel: 'WARN',
     },
-    // 在 local 或者 unittest 环境下，默认不缓存日志，直接写入磁盘
     buffer: false,
   },
 };
--- config/config.unittest.js
+++ config/config.unittest.js
@@ -2,7 +2,6 @@
 
 module.exports = {
   logger: {
-    // 默认不缓存日志，直接写入磁盘
     consoleLevel: 'WARN',
     buffer: false,
   },
