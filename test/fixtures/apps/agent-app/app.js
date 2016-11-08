ea71515a60f84a447cc37610f7b5dae56e6aa114
zōng yǔ <gxcsoccer@users.noreply.github.com>
2016-11-08 21:30:40 +0800

    test: refactor workclient test cases (#145)
    close egg#130

--- test/fixtures/apps/agent-app/app.js
+++ test/fixtures/apps/agent-app/app.js
@@ -1,11 +1,21 @@
 'use strict';
 
 module.exports = app => {
-  const done = app.readyCallback('foo');
-  app.mockClient.subscribe({
-    id: 'foo'
-  }, value => {
+  const fooDone = app.readyCallback('foo_sub_done');
+  const saveDone = app.readyCallback('mock_save_done');
+
+  function listener(value) {
     app.foo = value;
-    done();
-  });
+    app.mockClient.subscribe({ id: 'foo' }, value => {
+      if (value < app.foo) {
+        throw new Error('subscribe error');
+      }
+      setImmediate(() => { app.mockClient.unSubscribe({ id: 'foo' }) });
+    });
+    app.mockClient.unSubscribe({ id: 'foo' }, listener);
+    fooDone();
+  }
+
+  app.mockClient.subscribe({ id: 'foo' }, listener);
+  app.mockClient.saveCallback('hello', 'world', saveDone);
 };
--- test/fixtures/apps/agent-app/plugins/mock-client/app.js
+++ test/fixtures/apps/agent-app/plugins/mock-client/app.js
@@ -4,21 +4,45 @@ module.exports = app => {
   const options = app.config.mock;
 
   app.mockClient = app.createAppWorkerClient('mock', {
-    subscribe: function(reg, listner) {
-      this._subscribe(reg, listner);
-      return this;
+    on(event, listner) {
+      return this._on(event, listner);
     },
-
-    * getData(id) {
-      return yield this._invoke('getData', [id]);
+    once(event, listner) {
+      return this._once(event, listner);
     },
-
+    removeListener(event, listner) {
+      return this._removeListener(event, listner);
+    },
+    removeAllListeners(event) {
+      return this._removeAllListeners(event);
+    },
+    subscribe(reg, listner) {
+      return this._subscribe(reg, listner);
+    },
+    unSubscribe(reg, listner) {
+      return this._unSubscribe(reg, listner)
+    },
+    * getData(key) {
+      return yield this._invoke('getData', [key]);
+    },
     * getError() {
       return yield this._invoke('getError', []);
     },
-
-    * getDataGenerator(id) {
-      return yield this._invoke('getDataGenerator', [id]);
+    * getTimeout() {
+      return yield this._invoke('getTimeout', []);
+    },
+    * getDataGenerator(key) {
+      return yield this._invoke('getDataGenerator', [key]);
+    },
+    * save(key, value) {
+      return yield this._invoke('save', [key, value]);
+    },
+    saveCallback(key, value, callback) {
+      this._invoke('save', [key, value])
+        .then(callback, callback);
+    },
+    saveAsync(key, value) {
+      this._invokeOneway('save', [key, value]);
     },
   }, options);
 
