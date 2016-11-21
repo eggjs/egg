87f48de4b6d82bf74bb9eca68ce85cbdf05bfb4e
dead-horse <dead_horse@qq.com>
2016-11-21 23:44:29 +0800

    fix: already supported in egg-core

--- app/middleware/body_parser.js
+++ app/middleware/body_parser.js
@@ -1,15 +1,3 @@
 'use strict';
 
-const bodyParser = require('koa-bodyparser');
-const pathMatching = require('egg-path-matching');
-
-module.exports = function(options) {
-  const bodyParserMiddleware = bodyParser(options);
-  const match = pathMatching(options);
-  return function* (next) {
-    if (!options.enable) return yield next;
-    if (!match(this)) return yield next;
-
-    return yield bodyParserMiddleware.call(this, next);
-  };
-};
+module.exports = require('koa-bodyparser');
