57293a9189766c777aa948a5d107414d994c062d
popomore <sakura9515@gmail.com>
2017-02-07 17:51:16 +0800

    f

--- scripts/doc_travis.sh
+++ scripts/doc_travis.sh
@@ -7,7 +7,7 @@ SOURCE_BRANCH="master"
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-  echo "Skipping deploy; just doing a build."
+  echo "Skip deploy, TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST, TRAVIS_BRANCH=$TRAVIS_BRANCH"
   exit 0
 fi
 
