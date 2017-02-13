036cb80a8995ef70cb55bcc88eb0d9b25652bd1a
popomore <sakura9515@gmail.com>
2017-02-13 14:49:16 +0800

    f

--- test/fixtures/apps/logger-level-debug/app/router.js
+++ test/fixtures/apps/logger-level-debug/app/router.js
@@ -1,8 +1,13 @@
 'use strict';
 
+const sleep = require('ko-sleep');
+
+
 module.exports = app => {
   app.get('/', function*() {
     this.logger.debug('hi %s %s', this.method, this.url);
+    // wait for writing to file
+    yield sleep(1000);
     this.body = 'ok';
   });
 };
