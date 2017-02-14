4114ad8e8223b1eefdbf941c751eb9d0c9744eb2
dead-horse <dead_horse@qq.com>
2017-02-14 16:09:28 +0800

    f

--- test/fixtures/apps/dumpconfig-app/app.js
+++
@@ -1,9 +0,0 @@
-const sleep = require('ko-sleep');
-
-module.exports = app => {
-  app.config.dynamic = 1;
-  app.beforeStart(function*() {
-    yield sleep(1000);
-    app.config.dynamic = 2;
-  });
-};
