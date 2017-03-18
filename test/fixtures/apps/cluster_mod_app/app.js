984d7320881adf9420e5c7e49d62d5530ad887dd
zōng yǔ <gxcsoccer@users.noreply.github.com>
2017-03-18 23:38:49 +0800

    refactor: app.cluster auto bind this (#570)

--- test/fixtures/apps/cluster_mod_app/app.js
+++ test/fixtures/apps/cluster_mod_app/app.js
@@ -6,7 +6,8 @@ module.exports = function(app) {
   const done = app.readyCallback('register_client', {
     isWeakDep: app.config.runMode === 0,
   });
-  app.registryClient = app.cluster(RegistryClient).create();
+  const cluster = app.cluster;
+  app.registryClient = cluster(RegistryClient).create();
   app.registryClient.ready(done);
 
   app.registryClient.subscribe({
