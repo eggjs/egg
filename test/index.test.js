241b4e83c05e7086493564e536f5ce69d17dde0c
Yiyu He <dead-horse@users.noreply.github.com>
2017-04-11 15:58:21 +0800

    feat: expose egg.Controller and egg.Service (#741)

---
+++ test/index.test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const assert = require('assert');
+const egg = require('..');
+
+describe('test/index.test.js', () => {
+  it('should expose properties', () => {
+    assert.deepEqual(Object.keys(egg).sort(), [
+      'Agent',
+      'AgentWorkerLoader',
+      'AppWorkerLoader',
+      'Application',
+      'Controller',
+      'Service',
+      'startCluster',
+    ]);
+  });
+});
