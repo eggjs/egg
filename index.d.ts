bd85e74bf1a00918455d521073737a3266023b0a
Rwing <Rwing@rwing.cn>
2017-06-21 10:17:35 +0800

    add another properties of FileStream

--- index.d.ts
+++ index.d.ts
@@ -529,6 +529,20 @@ export interface Application extends EggApplication {
 
 interface FileStream extends Readable { // tslint:disable-line
   fields: any;
+
+  filename: string;
+
+  fieldname: string;
+
+  mime: string;
+
+  mimeType: string;
+
+  transferEncoding: string;
+
+  encoding: string;
+
+  truncated: boolean;
 }
 
 export interface Context extends KoaApplication.Context {
