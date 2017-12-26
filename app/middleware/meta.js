b0e3ae4e56848470ab57cd4600feddcb66a36214
fengmk2 <fengmk2@gmail.com>
2017-12-26 12:15:03 +0800

    feat: enable request started log on meta middleware

--- app/middleware/meta.js
+++ app/middleware/meta.js
@@ -4,8 +4,11 @@
 
 'use strict';
 
-module.exports = () => {
+module.exports = options => {
   return async function meta(ctx, next) {
+    if (options.logging) {
+      ctx.coreLogger.info('[meta] request started, host: %s, user-agent: %s', ctx.host, ctx.header['user-agent']);
+    }
     await next();
     // total response time header
     ctx.set('x-readtime', Date.now() - ctx.starttime);
