99e8c7ad6d464249dce3523d22147a89bac9ee23
whxaxes <whxaxes@gmail.com>
2018-01-19 15:14:54 +0800

    docs: update example

--- app/extend/helper.js
+++ app/extend/helper.js
@@ -14,7 +14,7 @@ module.exports = {
    * @example
    * ```js
    * app.get('home', '/index.htm', 'home.index');
-   * helper.pathFor('home', { by: 'recent', limit: 20 })
+   * ctx.helper.pathFor('home', { by: 'recent', limit: 20 })
    * => /index.htm?by=recent&limit=20
    * ```
    * @return {String} url path(without host)
@@ -31,7 +31,7 @@ module.exports = {
    * @example
    * ```js
    * app.get('home', '/index.htm', 'home.index');
-   * helper.urlFor('home', { by: 'recent', limit: 20 })
+   * ctx.helper.urlFor('home', { by: 'recent', limit: 20 })
    * => http://127.0.0.1:7001/index.htm?by=recent&limit=20
    * ```
    * @return {String} full url(with host)
--- index.d.ts
+++ index.d.ts
@@ -816,7 +816,7 @@ export interface IHelper {
    * @example
    * ```js
    * app.get('home', '/index.htm', 'home.index');
-   * helper.pathFor('home', { by: 'recent', limit: 20 })
+   * ctx.helper.pathFor('home', { by: 'recent', limit: 20 })
    * => /index.htm?by=recent&limit=20
    * ```
    * @return {String} url path(without host)
@@ -831,7 +831,7 @@ export interface IHelper {
    * @example
    * ```js
    * app.get('home', '/index.htm', 'home.index');
-   * helper.urlFor('home', { by: 'recent', limit: 20 })
+   * ctx.helper.urlFor('home', { by: 'recent', limit: 20 })
    * => http://127.0.0.1:7001/index.htm?by=recent&limit=20
    * ```
    * @return {String} full url(with host)
