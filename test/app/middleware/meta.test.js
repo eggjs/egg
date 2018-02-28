77254bea60fd7ec24b0a4da07a2a2d183fb42d12
fengmk2 <fengmk2@gmail.com>
2018-02-28 18:03:56 +0800

    feat: support Keep-Alive Header (#2146)
    https://github.com/node-modules/agentkeepalive/issues/58

--- test/app/middleware/meta.test.js
+++ test/app/middleware/meta.test.js
@@ -42,4 +42,30 @@ describe('test/app/middleware/meta.test.js', () => {
       assert(content.includes('[meta] request started, host: '));
     });
   });
+
+  describe('cluster start', () => {
+    let app;
+    before(() => {
+      app = utils.cluster('apps/middlewares');
+      return app.ready();
+    });
+    after(() => app.close());
+
+    it('should ignore keep-alive header when request is not keep-alive', () => {
+      return app.httpRequest()
+        .get('/')
+        .expect('X-Readtime', /\d+/)
+        .expect(res => assert(!res.headers['keep-alive']))
+        .expect(200);
+    });
+
+    it('should return keep-alive header when request is keep-alive', () => {
+      return app.httpRequest()
+        .get('/')
+        .set('connection', 'keep-alive')
+        .expect('X-Readtime', /\d+/)
+        .expect('keep-alive', 'timeout=5')
+        .expect(200);
+    });
+  });
 });
