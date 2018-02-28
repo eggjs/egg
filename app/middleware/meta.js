a7390023150ff4d5a7ec069276a94542a7ef67fa
fengmk2 <fengmk2@gmail.com>
2018-02-28 18:03:56 +0800

    feat: support Keep-Alive Header (#2146)
    https://github.com/node-modules/agentkeepalive/issues/58

--- app/middleware/meta.js
+++ app/middleware/meta.js
@@ -12,5 +12,12 @@ module.exports = options => {
     await next();
     // total response time header
     ctx.set('x-readtime', Date.now() - ctx.starttime);
+
+    // try to support Keep-Alive Header
+    const server = ctx.app.server;
+    if (server && server.keepAliveTimeout && server.keepAliveTimeout >= 1000 && ctx.header.connection !== 'close') {
+      const timeout = parseInt(server.keepAliveTimeout / 1000);
+      ctx.set('keep-alive', `timeout=${timeout}`);
+    }
   };
 };
