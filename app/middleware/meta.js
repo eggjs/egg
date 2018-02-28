1db421721a504ab52fecb5ac4681532f3b154caa
fengmk2 <fengmk2@gmail.com>
2018-02-28 16:52:18 +0800

    f

--- app/middleware/meta.js
+++ app/middleware/meta.js
@@ -4,12 +4,7 @@
 
 'use strict';
 
-module.exports = (options, app) => {
-  let server;
-  app.once('server', s => {
-    server = s;
-  });
-
+module.exports = options => {
   return async function meta(ctx, next) {
     if (options.logging) {
       ctx.coreLogger.info('[meta] request started, host: %s, user-agent: %s', ctx.host, ctx.header['user-agent']);
@@ -19,6 +14,7 @@ module.exports = (options, app) => {
     ctx.set('x-readtime', Date.now() - ctx.starttime);
 
     // try to support Keep-Alive Header
+    const server = ctx.app.server;
     if (server && server.keepAliveTimeout && server.keepAliveTimeout >= 1000 && ctx.header.connection !== 'close') {
       const timeout = parseInt(server.keepAliveTimeout / 1000);
       ctx.set('keep-alive', `timeout=${timeout}`);
