5f266a3add91d88d96f9c924977ddd7e97c3a82f
Haoliang Gao <sakura9515@gmail.com>
2018-03-05 18:10:37 +0800

    fix: don't allow x-forwarded-host header (#2163)
    It's a security issue, x-forwarded-host can be retreived
    from ctx.host when app.config.proxy is true, and be injected
    to cookie domain.

--- test/app/extend/request.test.js
+++ test/app/extend/request.test.js
@@ -32,10 +32,12 @@ describe('test/app/extend/request.test.js', () => {
         assert(req.host === '');
       });
 
-      it('should return host from X-Forwarded-Host header', () => {
+      it('should not allow X-Forwarded-Host header', () => {
+        mm(app.config, 'proxy', true);
         mm(req.header, 'x-forwarded-host', 'foo.com');
+        mm(req.header, 'host', 'bar.com');
         assert(typeof req.host === 'string');
-        assert(req.host === 'foo.com');
+        assert(req.host === 'bar.com');
       });
 
       it('should return host from Host header when proxy=false', () => {
@@ -44,6 +46,15 @@ describe('test/app/extend/request.test.js', () => {
         mm(req.header, 'host', 'bar.com');
         assert(typeof req.host === 'string');
         assert(req.host === 'bar.com');
+      });
+
+      it('should custom hostHeaders', () => {
+        mm(app.config, 'proxy', true);
+        mm(app.config, 'hostHeaders', 'x-forwarded-host');
+        mm(req.header, 'x-forwarded-host', 'foo.com');
+        mm(req.header, 'host', 'bar.com');
+        assert(typeof req.host === 'string');
+        assert(req.host === 'foo.com');
       });
     });
 
