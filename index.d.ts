c0d53fe3c1a11db4bc235d8d13e4c8d2d66959e8
waiting <1661926154@qq.com>
2018-03-30 11:34:56 +0800

    fix(typings): typo

--- index.d.ts
+++ index.d.ts
@@ -866,7 +866,7 @@ declare module 'egg' {
     https: boolean  // https or not
     key: string //ssl key
     cert: string  // ssl cert
-    typescrip: boolean
+    typescript: boolean
   }
 
   export function startCluster(options: ClusterOptions, callback: () => any): void
