0bddc7769d3a906a04c70acab31dffbdaecf7e82
dead-horse <dead_horse@qq.com>
2018-04-13 16:05:02 +0800

    f

--- test/app/extend/agent.test.js
+++ test/app/extend/agent.test.js
@@ -17,30 +17,30 @@ describe('test/app/extend/agent.test.js', () => {
     after(() => app.close());
 
     it('should add singleton success', async () => {
-      let config = await app.dataService.get('first').getConfig();
+      let config = await app.agent.dataService.get('first').getConfig();
       assert(config.foo === 'bar');
       assert(config.foo1 === 'bar1');
 
-      const ds = app.dataService.createInstance({ foo: 'barrr' });
+      const ds = app.agent.dataService.createInstance({ foo: 'barrr' });
       config = await ds.getConfig();
       assert(config.foo === 'barrr');
 
-      const ds2 = await app.dataService.createInstanceAsync({ foo: 'barrr' });
+      const ds2 = await app.agent.dataService.createInstanceAsync({ foo: 'barrr' });
       config = await ds2.getConfig();
       assert(config.foo === 'barrr');
 
-      config = await app.dataServiceAsync.get('first').getConfig();
+      config = await app.agent.dataServiceAsync.get('first').getConfig();
       assert(config.foo === 'bar');
       assert(config.foo1 === 'bar1');
 
       try {
-        app.dataServiceAsync.createInstance({ foo: 'barrr' });
+        app.agent.dataServiceAsync.createInstance({ foo: 'barrr' });
         throw new Error('should not execute');
       } catch (err) {
         assert(err.message === 'egg:singleton dataServiceAsync only support create asynchronous, please use createInstanceAsync');
       }
 
-      const ds4 = await app.dataServiceAsync.createInstanceAsync({ foo: 'barrr' });
+      const ds4 = await app.agent.dataServiceAsync.createInstanceAsync({ foo: 'barrr' });
       config = await ds4.getConfig();
       assert(config.foo === 'barrr');
     });
