3a0cec9175c4e8b718e5b7c7c07bac8c596cd603
popomore <sakura9515@gmail.com>
2018-05-02 16:49:40 +0800

    f

--- config/config.default.js
+++ config/config.default.js
@@ -318,7 +318,7 @@ module.exports = appInfo => {
    * @property {Number} responseTimeout - response timeout, default is 60000
    */
   config.clusterClient = {
-    maxIdleTime: 60000,
+    maxWaitTime: 60000,
     responseTimeout: 60000,
   };
 
