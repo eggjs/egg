14538a1fd059f5e24ca5ec7cdf606d51928c7d18
Yiyu He <dead_horse@qq.com>
2018-09-20 18:18:20 +0800

    deps: pin circular-json@0.5.5 to avoid output deprecate message (#3024)
    the author of `circular-json` recommended `flatted` to replace `circular-json`
    but `flatted`'s output is too different with JSON.stringify which don't fit our needs

--- .autod.conf.js
+++ .autod.conf.js
@@ -29,5 +29,9 @@ module.exports = {
   semver: [
     'koa-bodyparser@2',
   ],
+  keep: [
+    // circular-json > 0.5.5 will output some deprecate message, and we don't want to use flatted
+    'circular-json',
+  ],
   test: 'scripts',
 };
