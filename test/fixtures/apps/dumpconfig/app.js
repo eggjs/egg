580fc9b0ccd8b6c2c06a0cbdc76bb1762c2b3b36
Suyi Thonatos.Yang <thonatos.yang@gmail.com>
2019-07-18 11:47:42 +0800

    test: mv dumpConfig assert to fixtures/apps/dumpconfig/app.js

--- test/fixtures/apps/dumpconfig/app.js
+++ test/fixtures/apps/dumpconfig/app.js
@@ -1,9 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const assert = require('assert');
 const sleep = require('mz-modules/sleep');
 
+function readJson(p) {
+  return JSON.parse(fs.readFileSync(p));
+}
+
 module.exports = app => {
+  const baseDir = app.config.baseDir;
+  let json;
+
   app.config.dynamic = 1;
   app.beforeStart(function*() {
-    yield sleep(500);
+    // dumpConfig() dynamically
+    json = readJson(path.join(baseDir, 'run/application_config.json'));
+    assert(json.config.dynamic === 1, 'should dump in config');
+    json = readJson(path.join(baseDir, 'run/agent_config.json'));
+    assert(json.config.dynamic === 0, 'should dump in config');
+
+    yield sleep(2000);
     app.config.dynamic = 2;
   });
 };
