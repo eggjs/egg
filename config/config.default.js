143e130c3b431e0fd0837da746466ce3b708fa07
Suyi Thonatos.Yang <thonatos.yang@gmail.com>
2019-08-26 11:21:36 +0800

    feat: set body-parser limit to 1mb

--- config/config.default.js
+++ config/config.default.js
@@ -186,8 +186,9 @@ module.exports = appInfo => {
    * @property {String | RegExp | Function | Array} ignore - won't parse request body when url path hit ignore pattern, can not set `ignore` when `match` presented
    * @property {String | RegExp | Function | Array} match - will parse request body only when url path hit match pattern
    * @property {String} encoding - body's encoding type，default is utf8
-   * @property {String} formLimit - limit of the urlencoded body. If the body ends up being larger than this limit, a 413 error code is returned. Default is 100kb
-   * @property {String} jsonLimit - limit of the json body, default is 100kb
+   * @property {String} formLimit - limit of the urlencoded body. If the body ends up being larger than this limit, a 413 error code is returned. Default is 1mb
+   * @property {String} jsonLimit - limit of the json body, default is 1mb
+   * @property {String} textLimit - limit of the text body, default is 1mb
    * @property {Boolean} strict - when set to true, JSON parser will only accept arrays and objects. Default is true
    * @property {Number} queryString.arrayLimit - urlencoded body array's max length, default is 100
    * @property {Number} queryString.depth - urlencoded body object's max depth, default is 5
@@ -196,8 +197,9 @@ module.exports = appInfo => {
   config.bodyParser = {
     enable: true,
     encoding: 'utf8',
-    formLimit: '100kb',
-    jsonLimit: '100kb',
+    formLimit: '1mb',
+    jsonLimit: '1mb',
+    textLimit: '1mb',
     strict: true,
     // @see https://github.com/hapijs/qs/blob/master/lib/parse.js#L8 for more options
     queryString: {
