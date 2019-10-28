007aa5adf6b1ea3af43eee7797606cbd4367a674
丁徐达 <xuda.ding@alibaba-inc.com>
2019-10-28 11:00:26 +0800

    fix: index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -468,7 +468,7 @@ declare module 'egg' {
 
     static: {
       prefix: string;
-      dir: string | string[];
+      dir: string | ({prefix: string, dir: string} | string)[];
       // support lazy load
       dynamic: boolean;
       preload: boolean;
