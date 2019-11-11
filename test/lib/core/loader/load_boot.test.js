a08450b4f69816063624717fe5d9177a1545687e
Xin Wang <wangxinalex@gmail.com>
2019-11-11 09:53:26 +0800

    Update test/lib/core/loader/load_boot.test.js
    Co-Authored-By: TZ | 天猪 <atian25@qq.com>

--- test/lib/core/loader/load_boot.test.js
+++ test/lib/core/loader/load_boot.test.js
@@ -14,7 +14,6 @@ describe('test/lib/core/loader/load_boot.test.js', () => {
   });
 
   it('should load app.js', async () => {
-    app.mockLog(app.coreLogger);
     await app.close();
     app.expectLog('app is ready');
 
