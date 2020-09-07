bd5cb2d52cb4176914929a6dead675c5b88149de
TZ <atian25@qq.com>
2020-09-07 10:28:42 +0800

    fix: only set keep-alive header before Node.js 14.8.0

--- app/middleware/meta.js
+++ app/middleware/meta.js
@@ -4,7 +4,12 @@
 
 'use strict';
 
+const semver = require('semver');
+
 module.exports = options => {
+  // Node.js >=14.8.0 will set Keep-Alive Header, see https://github.com/nodejs/node/pull/34561
+  const shouldPatchKeepAliveHeader = semver.lt(process.version, '14.8.0');
+
   return async function meta(ctx, next) {
     if (options.logging) {
       ctx.coreLogger.info('[meta] request started, host: %s, user-agent: %s', ctx.host, ctx.header['user-agent']);
@@ -13,9 +18,9 @@ module.exports = options => {
     // total response time header
     ctx.set('x-readtime', Date.now() - ctx.starttime);
 
-    // try to support Keep-Alive Header
+    // try to support Keep-Alive Header when < 14.8.0
     const server = ctx.app.server;
-    if (server && server.keepAliveTimeout && server.keepAliveTimeout >= 1000 && ctx.header.connection !== 'close') {
+    if (shouldPatchKeepAliveHeader && server && server.keepAliveTimeout && server.keepAliveTimeout >= 1000 && ctx.header.connection !== 'close') {
       /**
        * use Math.floor instead of parseInt. More: https://github.com/eggjs/egg/pull/2702
        */
