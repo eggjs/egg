2e942053c05d7d0114aeee1d22605146fd93bc43
sky <hubcarl@126.com>
2020-09-08 08:35:12 +0800

    feat: egg 1.x support cookies config init (#4444)

--- test/lib/plugins/i18n.test.js
+++ test/lib/plugins/i18n.test.js
@@ -1,5 +1,4 @@
 'use strict';
-
 const utils = require('../../utils');
 
 describe('test/lib/plugins/i18n.test.js', () => {
@@ -15,7 +14,7 @@ describe('test/lib/plugins/i18n.test.js', () => {
       return app.httpRequest()
         .get('/message?locale=de')
         .expect(200)
-        .expect('Set-Cookie', /locale=de; path=\/; expires=[^;]+ GMT/)
+        .expect('Set-Cookie', /locale=de; path=\/; max-age=\d+; expires=[^;]+ GMT/)
         .expect({
           message: 'Hallo fengmk2, wie geht es dir heute? Wie war dein 18.',
           empty: '',
@@ -36,7 +35,7 @@ describe('test/lib/plugins/i18n.test.js', () => {
       return app.httpRequest()
         .get('/')
         .expect(200)
-        .expect('Set-Cookie', /locale=en-us; path=\/; expires=[^;]+ GMT/)
+        .expect('Set-Cookie', /locale=en-us; path=\/; max-age=\d+; expires=[^;]+ GMT/)
         .expect('<li>Email: </li>\n<li>Hello fengmk2, how are you today?</li>\n<li>foo bar</li>\n');
     });
 
@@ -45,7 +44,7 @@ describe('test/lib/plugins/i18n.test.js', () => {
         .get('/?locale=zh_CN')
         .set('Host', 'foo.example.com')
         .expect(200)
-        .expect('Set-Cookie', /locale=zh-cn; path=\/; expires=[^;]+ GMT/)
+        .expect('Set-Cookie', /locale=zh-cn; path=\/; max-age=\d+; expires=[^;]+ GMT/)
         .expect('<li>邮箱: </li>\n<li>fengmk2，今天过得如何？</li>\n<li>foo bar</li>\n');
     });
   });
