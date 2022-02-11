51c7bc69a5878f6941bb3abd2c341935c60223c6
Suyi <thonatos.yang@gmail.com>
2022-02-11 16:50:17 +0800

    docs: support jsdoc

---
+++ scripts/jsdoc.js
@@ -0,0 +1,17 @@
+const co = require('co');
+const path = require('path');
+const jsdoc = require('egg-doctools/lib/jsdoc');
+
+
+co(function* () {
+
+  const baseDir = process.cwd();
+  const target = path.resolve(__dirname, '../docs/dist/api')
+
+  console.log('#jsdoc: generate from', baseDir);
+  console.log('#jsdoc: generate to', target);
+
+  yield jsdoc({ baseDir, target });  
+
+  console.log('#jsdoc: done');
+});
