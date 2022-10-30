d820b739b95207bdea8c9b4c3da0f5059bc0113c
fengmk2 <fengmk2@gmail.com>
2022-10-30 22:26:05 +0800

    👌 IMPROVE: Don't need to patch keep-alive header on Node.js >= 14.20.0 (#5051)

--- app/middleware/meta.js
+++ app/middleware/meta.js
@@ -2,15 +2,9 @@
  * meta middleware, should be the first middleware
  */
 
-'use strict';
-
 const { performance } = require('perf_hooks');
-const semver = require('semver');
 
 module.exports = options => {
-  // Node.js >=14.8.0 and >= 12.19.0 will set Keep-Alive Header, see https://github.com/nodejs/node/pull/34561
-  const shouldPatchKeepAliveHeader = !semver.satisfies(process.version, '>=14.8.0 || ^12.19.0');
-
   return async function meta(ctx, next) {
     if (options.logging) {
       ctx.coreLogger.info('[meta] request started, host: %s, user-agent: %s', ctx.host, ctx.header['user-agent']);
@@ -21,16 +15,6 @@ module.exports = options => {
       ctx.set('x-readtime', Math.floor((performance.now() - ctx.performanceStarttime) * 1000) / 1000);
     } else {
       ctx.set('x-readtime', Date.now() - ctx.starttime);
-    }
-
-    // try to support Keep-Alive Header when < 14.8.0
-    const server = ctx.app.server;
-    if (shouldPatchKeepAliveHeader && server && server.keepAliveTimeout && server.keepAliveTimeout >= 1000 && ctx.header.connection !== 'close') {
-      /**
-       * use Math.floor instead of parseInt. More: https://github.com/eggjs/egg/pull/2702
-       */
-      const timeout = Math.floor(server.keepAliveTimeout / 1000);
-      ctx.set('keep-alive', `timeout=${timeout}`);
     }
   };
 };
