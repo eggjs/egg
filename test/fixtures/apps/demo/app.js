2690e08ba5c8302642eb616d0adc86b3bbed252e
William.Tung <maledong_public@foxmail.com>
2023-06-05 21:44:04 +0800

    test: fix and enable 'egg.test.js' (#5200)
    Bug Reasons:
    
    1. We cannot get "messenger" and "lifecycle" instances belonging to "app"
    itself before "ready()" function, a "cannot get xxx property" will be
    raised.
    
    2. We CANNOT use "mm(……)" to replace "triggerServerDidReady" function,
    after "app.ready()", bcoz it WON'T be triggered if we do that. The only
    solution is to replace it in the constructor.

--- test/fixtures/apps/demo/app.js
+++ test/fixtures/apps/demo/app.js
@@ -1,9 +1,31 @@
 'use strict';
+const mm = require('egg-mock');
+const assert = require('assert');
+const utils = require('../../../utils');
 
-module.exports = app => {
-  app.ready(() => {
-    app.config.tips = 'hello egg started';
+class DemoAppTest {
+
+  constructor(app) {
+    this.app = app;
+
+    // Add an attached variable exposed to the unit test
+    this.app.triggerCount = 0;
+
+    // Mock "lifecycle" function with a counter
+    // Before calling "ready()"
+    mm(this.app.lifecycle, 'triggerServerDidReady', () => {
+      this.app.triggerCount++;
+    });
+  }
+
+  configWillLoad() {
+    this.app.config.tips = 'hello egg started';
+  }
+
+  async didReady() {
     // dynamic router
-    app.all('/all', app.controller.home);
-  });
-};
+    this.app.all('/all', this.app.controller.home);
+  }
+}
+
+module.exports = DemoAppTest;
