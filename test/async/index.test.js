f08f19c9a511532c29a0f11fe38e8c56217a4808
MaledongGit <maledong_public@foxmail.com>
2023-06-07 14:39:08 +0800

    test: remove useless "async" support check
    Our current nodejs version MUST BE >=14.20.0, no need to check whether
    the current version satisfies the test env.

--- test/async/index.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const nodeVersion = Number(process.version.match(/^v(\d+\.\d+)\./)[1]);
-// only node >= 7.6 supports async function without flags
-if (nodeVersion >= 7.6) {
-  require('./_async');
-}
