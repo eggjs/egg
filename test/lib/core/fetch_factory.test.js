6c36721fb06b6cd614e4a4d1e2ac6f02a7c8ac4e
killagu <zhubin.gzb@antgroup.com>
2025-07-03 18:43:51 +0800

    f

--- test/lib/core/fetch_factory.test.js
+++ test/lib/core/fetch_factory.test.js
@@ -1,6 +1,6 @@
 const assert = require('node:assert');
 const mm = require('egg-mock');
-const FetchFactory = require('../../../lib/core/fetch_factory');
+const { FetchFactory, safeFetch } = require('../../../lib/core/fetch_factory');
 const utils = require('../../utils');
 
 describe('test/lib/core/fetch_factory.test.js', () => {
@@ -16,5 +16,30 @@ describe('test/lib/core/fetch_factory.test.js', () => {
   it('should fetch ok', async () => {
     const { status } = await FetchFactory.fetch(url);
     assert(status === 200);
+  });
+
+  it('should support safeFetch', async () => {
+    let ip;
+    let family;
+    let host;
+    const fetch = safeFetch.bind({
+      config: {
+        security: {
+          ssrf: {
+            checkAddress(aIp, aFamily, aHost) {
+              ip = aIp;
+              family = aFamily;
+              host = aHost;
+              return true;
+            },
+          },
+        },
+      },
+      logger: console,
+    });
+    await fetch(url);
+    assert(ip);
+    assert(family);
+    assert(host);
   });
 });
