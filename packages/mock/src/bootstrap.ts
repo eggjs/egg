0fbb3362369febcb054eef24ff0cf0752728ae33
MK <fengmk2@gmail.com>
2025-09-14 09:51:28 +0800

    chore(supertest): remove nock devDeps

--- packages/mock/src/bootstrap.ts
+++ packages/mock/src/bootstrap.ts
@@ -2,25 +2,20 @@ import { strict as assert } from 'node:assert';
 import path from 'node:path';
 
 import { readJSONSync } from 'utility';
-import mm, { mock, MockApplication } from './index.ts';
+import mm, { mock, type MockApplication } from './index.ts';
 import { getBootstrapApp, setupApp } from './lib/app_handler.ts';
 import { getEggOptions } from './lib/utils.ts';
 
 const options = getEggOptions();
 
 // throw error when an egg plugin test is using bootstrap
-const pkgInfo = readJSONSync(path.join(options.baseDir || process.cwd(), 'package.json'));
+const pkgInfo = readJSONSync(
+  path.join(options.baseDir || process.cwd(), 'package.json')
+);
 if (pkgInfo.eggPlugin) {
   throw new Error('DO NOT USE bootstrap to test plugin');
 }
 
 const app = setupApp();
 
-export {
-  assert,
-  getBootstrapApp,
-  app,
-  mm,
-  mock,
-  MockApplication,
-};
+export { assert, getBootstrapApp, app, mm, mock, type MockApplication };
