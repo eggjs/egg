873e33162dc1e7e0c4d17d63b0e497f02b9b6544
MK <fengmk2@gmail.com>
2025-09-21 15:53:41 +0800

    FIXUP

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    exclude: ['tools/egg-bin/**'],
     projects: [
       'packages/*',
       'plugins/*',
