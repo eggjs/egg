b08ba16c05fc4ae2fed5b86c94c9a8045a48eb1f
MK <fengmk2@gmail.com>
2025-09-25 21:12:09 +0800

    FIXUP

--- tools/create-egg/src/templates/tegg/test/setup.ts
+++ tools/create-egg/src/templates/tegg/test/setup.ts
@@ -2,8 +2,4 @@ import { beforeAll, afterAll } from 'vitest';
 
 // https://vitest.dev/config/#setupfiles
 // export beforeAll and afterAll to globalThis, let @eggjs/mock/bootstrap use it
-
-// @ts-expect-error globalThis is not typed
-globalThis.beforeAll = beforeAll;
-// @ts-expect-error globalThis is not typed
-globalThis.afterAll = afterAll;
+Object.assign(globalThis, { beforeAll, afterAll });
