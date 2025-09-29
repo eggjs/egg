65a852639e8122162a67d5ad10ef169a4e60a55b
MK <fengmk2@gmail.com>
2025-09-29 14:40:19 +0800

    fix the port:34475 is occupied by other

--- plugins/watcher/src/lib/boot.ts
+++ plugins/watcher/src/lib/boot.ts
@@ -1,25 +1,21 @@
-import type { ILifecycleBoot, Application } from 'egg';
+import type { ILifecycleBoot, EggApplicationCore } from 'egg';
 
 import { Watcher } from './watcher.ts';
 
 export class Boot implements ILifecycleBoot {
-  #app: Application;
-  #watcher: Watcher;
+  #app: EggApplicationCore;
 
-  constructor(appOrAgent: Application) {
+  constructor(appOrAgent: EggApplicationCore) {
     this.#app = appOrAgent;
-    this.#watcher = this.#app.watcher = this.#app
-      .cluster(Watcher, {})
-      .delegate('watch', 'subscribe')
-      .create(appOrAgent.config);
-    this.#watcher
+    this.#app.watcher = this.#app.clusterWrapper(Watcher, {}).delegate('watch', 'subscribe').create(appOrAgent.config);
+    this.#app.watcher
       .on('info', (msg: string, ...args: any[]) => this.#app.coreLogger.info(msg, ...args))
       .on('warn', (msg: string, ...args: any[]) => this.#app.coreLogger.warn(msg, ...args))
       .on('error', (msg: string, ...args: any[]) => this.#app.coreLogger.error(msg, ...args));
   }
 
   async didLoad(): Promise<void> {
-    await this.#watcher.ready();
+    await this.#app.watcher.ready();
     this.#app.coreLogger.info('[@eggjs/watcher:%s] watcher start success', this.#app.type);
   }
 }
--- plugins/watcher/src/types.ts
+++ plugins/watcher/src/types.ts
@@ -2,11 +2,7 @@ import type { Watcher } from './lib/watcher.ts';
 import type { WatcherConfig } from './config/config.default.ts';
 
 declare module 'egg' {
-  interface Application {
-    watcher: Watcher;
-  }
-
-  interface Agent {
+  interface EggApplicationCore {
     watcher: Watcher;
   }
 
