51fa100451af9aa2cd22cf8d9b73bd8bea327e31
MK <fengmk2@gmail.com>
2025-10-13 22:18:33 +0800

    FIXUP

--- plugins/typebox-validate/src/decorator.ts
+++ plugins/typebox-validate/src/decorator.ts
@@ -7,9 +7,14 @@ type CustomErrorMessage = (ctx: Context, errors: ErrorObject[]) => string;
 type GetData = (ctx: Context, args: unknown[]) => unknown;
 export type ValidateRule = [TSchema, GetData, CustomErrorMessage?];
 
+export type ValidateDecorator = (rules: ValidateRule[]) => MethodDecorator;
+
+/**
+ * Validate decorator factory
+ */
 export function ValidateFactory(
   customHandler: (ctx: Context, data?: unknown, schema?: TSchema, customError?: CustomErrorMessage) => void
-) {
+): ValidateDecorator {
   return function Validate(rules: ValidateRule[]): MethodDecorator {
     return (_, __, descriptor: PropertyDescriptor): PropertyDescriptor => {
       const fn = descriptor.value;
@@ -30,15 +35,25 @@ export function ValidateFactory(
   };
 }
 
-export const Validate: (rules: ValidateRule[]) => MethodDecorator = ValidateFactory(
-  (ctx, data, schema, customError) => {
-    const app = ctx.app;
-    const message = customError ? customError(ctx, app.ajv.errors!) : 'Validation Failed';
-    ctx.throw(422, message, {
-      code: 'invalid_param',
-      errorData: data,
-      currentSchema: JSON.stringify(schema),
-      errors: app.ajv.errors,
-    });
-  }
-);
+/**
+ * Validate decorator
+ *
+ * @example
+ * ```ts
+ * @Validate([[paramsSchema, ctx => ctx.params]])
+ * async index() {
+ *   const { ctx } = this;
+ *   ctx.body = ctx.params;
+ * }
+ * ```
+ */
+export const Validate: ValidateDecorator = ValidateFactory((ctx, data, schema, customError) => {
+  const app = ctx.app;
+  const message = customError ? customError(ctx, app.ajv.errors!) : 'Validation Failed';
+  ctx.throw(422, message, {
+    code: 'invalid_param',
+    errorData: data,
+    currentSchema: JSON.stringify(schema),
+    errors: app.ajv.errors,
+  });
+});
