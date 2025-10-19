13ab91c6bb50e99290e5e5ae09c021bd8531283e
MK <fengmk2@gmail.com>
2025-10-19 15:22:11 +0800

    FIXUP

--- tegg/core/dal-runtime/test/fixtures/modules/dal/dal/extension/FooExtension.ts
+++
@@ -1,8 +0,0 @@
-import { SqlMap, SqlType } from '@eggjs/dal-decorator';
-
-export default {
-  findAll: {
-    type: SqlType.SELECT,
-    sql: 'SELECT {{ allColumns}} from egg_foo;',
-  },
-} as Record<string, SqlMap>;
--- tegg/core/dal-runtime/test/fixtures/modules/generate_codes/dal/structure/MultiPrimaryKey.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS multi_primary_key_table (
-  id_1 INT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'the primary key',
-  id_2 INT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'the primary key',
-  name VARCHAR(100) NOT NULL UNIQUE KEY
-) COMMENT='multi primary key table';
