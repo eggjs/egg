970537b3f5d14babb0007f96860bf3a5a55856c1
MK <fengmk2@gmail.com>
2025-10-22 19:06:27 +0800

    FIXUP

--- tools/scripts/test/stop.test.ts
+++ tools/scripts/test/stop.test.ts
@@ -18,7 +18,7 @@ describe('test/stop.test.ts', () => {
   const timeoutPath = path.join(__dirname, 'fixtures/stop-timeout');
   const homePath = path.join(__dirname, 'fixtures/home');
   const logDir = path.join(homePath, 'logs');
-  const waitTime = 500;
+  const waitTime = 1000;
 
   beforeAll(async () => {
     await fs.mkdir(homePath, { recursive: true });
