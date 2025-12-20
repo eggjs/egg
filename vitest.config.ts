393e0ec5ecdb246498eccda83f5bf7facb77f913
MK <fengmk2@gmail.com>
2025-12-20 23:30:04 +0800

    FIXUP

--- vitest.config.ts
+++ vitest.config.ts
@@ -23,6 +23,7 @@ const config: UserWorkspaceConfig = defineConfig({
       exclude: ['**/test/**'],
     },
     hookTimeout: 20000,
+    testTimeout: 20000,
     env: {
       // disable tegg plugins by default on unittest, make test speed up
       DISABLE_TEGG_PLUGINS: 'true',
