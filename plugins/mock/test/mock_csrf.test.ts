14d70ec324c1648ee83f6e4576484a876820136f
MK <fengmk2@gmail.com>
2025-12-20 15:52:43 +0800

    FIXUP

--- plugins/mock/test/mock_csrf.test.ts
+++ plugins/mock/test/mock_csrf.test.ts
@@ -3,7 +3,8 @@ import { describe, it, beforeAll, afterAll, afterEach } from 'vitest';
 import mm, { type MockApplication } from '../src/index.ts';
 import { getFixtures } from './helper.ts';
 
-describe('test/mock_csrf.test.ts', () => {
+// TODO: flaky test on windows, Error: EPERM: operation not permitted
+describe.skipIf(process.platform === 'win32')('test/mock_csrf.test.ts', () => {
   let app: MockApplication;
   beforeAll(async () => {
     app = mm.app({
