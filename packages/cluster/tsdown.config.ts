df7bb7b77895fa87f65179f9b2ffd83cb60c8a18
MK <fengmk2@gmail.com>
2025-12-20 18:00:30 +0800

    FIXUP

--- packages/cluster/tsdown.config.ts
+++ packages/cluster/tsdown.config.ts
@@ -3,6 +3,8 @@ import { defineConfig, type UserConfig } from 'tsdown';
 const config: UserConfig = defineConfig({
   entry: {
     index: 'src/index.ts',
+    agent_worker: 'src/agent_worker.ts',
+    app_worker: 'src/app_worker.ts',
   },
 });
 
--- plugins/development/tsdown.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig, type UserConfig } from 'tsdown';
-
-const config: UserConfig = defineConfig({
-  entry: {
-    index: 'src/index.ts',
-  },
-});
-
-export default config;
