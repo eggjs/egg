7161a4a1060cb6213039bf7bb9d1bd8e01872357
Dipper30 <ddddipper30@gmail.com>
2026-01-09 18:14:34 +0800

    feat: add dns error test case

--- test/lib/core/dns_resolver.test.js
+++ test/lib/core/dns_resolver.test.js
@@ -1,4 +1,7 @@
 const utils = require('../../utils');
+const mm = require('egg-mock');
+const dns = require('dns');
+const dnsPromise = require('dns').promises;
 const assert = require('assert');
 
 describe('test/lib/core/dns_resolver.test.js', () => {
@@ -21,6 +24,8 @@ describe('test/lib/core/dns_resolver.test.js', () => {
     url2 = server2.url;
     url2 = url2.replace('127.0.0.2', 'localhost');
   });
+
+  afterEach(mm.restore);
 
   after(() => {
     if (server1?.server?.listening) server1.server.close();
@@ -76,4 +81,69 @@ describe('test/lib/core/dns_resolver.test.js', () => {
       assert(err.message.includes('fetch failed'));
     }
   });
+
+  it('should not fail even if dns.lookup fail, because lookup is overridden', async () => {
+    mm.error(dnsPromise, 'lookup', 'mock dns lookup error');
+    const res = await app.curl(url2 + '/get_headers', { dataType: 'json' });
+    assert(res.status === 200);
+  });
 });
+
+describe('test/lib/core/dns_resolver with dns error', () => {
+  let server;
+  let cache = new Map()
+  before(async () => {
+    server = await utils.startNewLocalServer('127.0.0.1');
+    if (!server) {
+      throw new Error('start local server failed');
+    }
+    app = utils.app('apps/dns_resolver');
+    await app.ready();
+    app.config.httpclient.lookup = function (hostname, options, callback) {
+      if (cache.has(hostname)) {
+        const address = cache.get(hostname);
+        callback(null, address, 4);
+        return;
+      } else {
+        dns.lookup(hostname, options, (err, address, family) => {
+          if (!err) {
+            cache.set(hostname, address);
+            callback(null, address, family);
+          } else {
+            callback(err);
+          }
+        });
+      }
+    }
+    url = server.url;
+    url = url.replace('127.0.0.1', 'localhost');
+  });
+
+  afterEach(mm.restore);
+
+  after(() => {
+    if (server?.server?.listening) server.server.close();
+  });
+
+  it('should curl work', async () => {
+    const res = await app.curl(url + '/get_headers', { dataType: 'json' });
+    assert(res.status === 200);
+    assert(cache.has('localhost'));
+  })
+
+  it('should cache work when dns fails', async () => {
+    mm.error(dns, 'lookup', 'mock dns lookup error');
+    const res = await app.curl(url + '/get_headers', { dataType: 'json' });
+    assert(res.status === 200);
+    assert(cache.has('localhost'));
+    cache.delete('localhost');
+    // should fail now
+    try {
+      await app.curl(url + '/get_headers', { dataType: 'json' });
+    } catch (err) {
+      assert(err);
+      assert(err.message.includes('mock dns lookup error'));
+    }
+  })
+})
+
