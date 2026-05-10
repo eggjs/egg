e9eff2bcb747c971408cceed2619f6d9f38f7fda
killa <killa07071201@gmail.com>
2026-05-10 15:09:22 +0800

    fix: declare bundle module loader global

--- packages/core/src/global.ts
+++ packages/core/src/global.ts
@@ -3,6 +3,7 @@ import type { ManifestStore } from './loader/manifest.ts';
 declare global {
   var __EGG_BUNDLE_STORE__: ManifestStore | undefined;
   var __EGG_BUNDLE_FILE_LOADER__: ((filepath: string) => string | undefined) | undefined;
+  var __EGG_BUNDLE_MODULE_LOADER__: ((filepath: string) => unknown) | undefined;
 }
 
 export {};
