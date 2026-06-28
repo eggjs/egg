c8e385140ef917ab75c027ecc982af828707f8f6
killa <killa07071201@gmail.com>
2026-06-28 17:12:15 +0800

    fix(ci): skip oclif shell probe in egg-bin

--- tools/egg-bin/test/fixtures/my-egg-bin/bin/run.js
+++ tools/egg-bin/test/fixtures/my-egg-bin/bin/run.js
@@ -1,5 +1,13 @@
 #!/usr/bin/env node
 
-import { execute } from '@oclif/core';
+function presetWindowsShell() {
+  if (process.platform !== 'win32' || process.env.SHELL) return;
+
+  const comspec = process.env.ComSpec ?? process.env.COMSPEC;
+  process.env.SHELL = comspec?.split(/[\\/]/).at(-1) || 'cmd.exe';
+}
+
+presetWindowsShell();
+const { execute } = await import('@oclif/core');
 
 await execute({ dir: import.meta.url });
