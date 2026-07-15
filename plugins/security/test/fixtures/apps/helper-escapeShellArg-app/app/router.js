6ef8736473d898fbb73bd0b170ede8966c6b01b8
killagu <zhubin.gzb@antgroup.com>
2026-07-15 20:34:45 +0800

    fix(security): escape shell arg single quotes

--- plugins/security/test/fixtures/apps/helper-escapeShellArg-app/app/router.js
+++ plugins/security/test/fixtures/apps/helper-escapeShellArg-app/app/router.js
@@ -10,7 +10,7 @@ module.exports = (app) => {
     const port = "8889'|chmod 777 /tmp/muma.sh;echo \\";
     this.body =
       `cp.exec('./start.sh '+${this.helper.escapeShellArg(port)})` ===
-      "cp.exec('./start.sh '+'8889\\'|chmod 777 /tmp/muma.sh;echo \\\\')";
+      "cp.exec('./start.sh '+'8889'\\''|chmod 777 /tmp/muma.sh;echo \\')";
   });
 
   app.get('/escapeShellArg-3', async function () {
